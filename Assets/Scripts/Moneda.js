4d742cd75a5d7a2069913d88e2d1d8b3387dfa5d
proyectounity25 <proyectounity25@gmail.com>
2015-06-14 12:22:03 +0200

    Rotacion moneda

---
+++ Assets/Scripts/Moneda.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var timer2 : float = 3.0; 
+
+function Start () {
+	//transform.parent = GameObject.Find("Cylinder").transform;
+}
+     
+function Update()
+{  
+	timer2 -= Time.deltaTime;
+	if (timer2 <= 0){
+	
+	} else {
+		//transform.Rotate(Vector3(5.0, -5.0, 7.0));
+		transform.rotation = Random.rotation;
+	}
+}
